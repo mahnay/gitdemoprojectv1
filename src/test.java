883983ae6107cc4772ddc085d69a716c30676900
MAHESH <MAHESH@DESKTOP-NM4O2CD>
2022-03-27 05:41:05 +0530

    first

--- src/test.java
+++ src/test.java
@@ -3,6 +3,14 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		{
+			String hungry = " yes ";
+			if ( hungry == "yes ")
+				System.out.println("eat something...");
+			else {
+				System.out.println("do work...");
+			}
+		}
 
 	}
 
